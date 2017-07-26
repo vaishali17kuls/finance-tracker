72fa36a6511ca60028a3b5fd59083517b74049e2
Rails udemy <rails@gmail.com>
2017-07-26 16:59:09 +0000

    make app prod easy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome/home'
+  root 'welcome#home'
   #get 'welcome/home', to: 'welcome#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
