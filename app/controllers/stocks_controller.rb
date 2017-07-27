0317df67f4a8bdf9d0b35ffdd1d0d3b77214caad
Rails udemy <rails@gmail.com>
2017-07-27 18:15:23 +0000

    added spinner

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,14 +1,13 @@
 class StocksController < ApplicationController
   def search
     if params[:stock]
-      @stock = Stock.find_by_ticker(params[:stock])
-      @stock ||= Stock.new_from_lookup(params[:stock])
+      @stock = Stock.find_by_ticker(params[:stock])         #find in d database
+      @stock ||= Stock.new_from_lookup(params[:stock])      #find in ol stocks
     end
     if @stock
       render partial: 'lookup'
       else
         render status: :not_found, nothing: true
-      end
     end
-
+  end
 end
